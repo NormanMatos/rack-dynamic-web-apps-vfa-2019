d432e53328586731df71cbabc4ac8c52fc68fee3
Norman Matos <nmatos1100@gmail.com>
2019-06-20 13:25:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,18 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+
+    num_1 = Kerne1.rand(1..3)
+    num_2 = Kernel.rand(1..3)
+    num_3 = Kernel.rand(1..3)
+
+    resp.write "#{num_1}\n"
+
+
     resp.write "Hello, World"
+
+
+
     resp.finish
   end
 
